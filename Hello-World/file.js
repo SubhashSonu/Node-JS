be7d05c343ebf19e49280fbc0e9aeb40090577d6
Subhash Kumar <subhashsonu7660@gmail.com>
2025-06-08 15:47:17 +0530

    Node Js

--- Hello-World/file.js
+++ Hello-World/file.js
@@ -31,4 +31,28 @@ const fs = require('fs');
 
 // console.log(fs.statSync("./test.txt"));
 
-console.log(fs.statSync("./test.txt").isFile())
+// console.log(fs.statSync("./test.txt").isFile())
+
+// sync - blocking
+console.log(1);
+const result = fs.readFileSync("./contacts.txt","utf-8");
+console.log(result);
+console.log(2);
+
+
+// async - non blocking
+console.log(1);
+fs.readFile("./contacts.txt","utf-8",(error,result)=>{
+    if(error){
+        console.log("Error is detected :",error);
+    }
+
+    else console.log(result);
+});
+
+console.log(2);
+
+const os = require("os");
+console.log("CPU cores:", os.cpus().length);
+
+
